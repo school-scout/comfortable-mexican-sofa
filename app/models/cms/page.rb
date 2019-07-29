f3a9953cc259391542f79eebd5fae7bfb956e31a
Martin Honermeyer <maze@strahlungsfrei.de>
2019-07-29 18:10:27 +0200

    Add meta_description field to pages

--- app/models/cms/page.rb
+++ app/models/cms/page.rb
@@ -13,7 +13,8 @@ class Cms::Page < ActiveRecord::Base
                   :blocks_attributes,
                   :is_published,
                   :target_page_id,
-                  :category_ids
+                  :category_ids,
+                  :meta_description
   
   cms_acts_as_tree :counter_cache => :children_count
   cms_is_categorized
